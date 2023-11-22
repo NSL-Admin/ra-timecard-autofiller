4dd295f6c695851b7ebe3753febda3e5045b12c5
t0d4 <todatakaaki@gmail.com>
2023-11-22 20:37:15 +0900

    fix: changed icon sizes

--- manifest.config.ts
+++ manifest.config.ts
@@ -3,16 +3,16 @@ import { defineManifest } from '@crxjs/vite-plugin'
 export const manifest = defineManifest({
   manifest_version: 3,
   name: 'RA Timecard Autofiller',
-  description: 'Help RAs fill in the timecard on My Waseda',
-  version: '1.0',
+  description: 'Help RAs fill in the timecard on MyWaseda',
+  version: '0.1.0',
   icons: {
     16: 'icons/icon-16.png',
-    38: 'icons/icon-38.png',
+    38: 'icons/icon-32.png',
     48: 'icons/icon-48.png',
     128: 'icons/icon-128.png',
   },
   action: {
-    default_icon: "icons/icon-128.png",
+    default_icon: { 48: 'icons/icon-48.png' },
     default_title: "RA Timecard Autofiller",
   },
   background: {
